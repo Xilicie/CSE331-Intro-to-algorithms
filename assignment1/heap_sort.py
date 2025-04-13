8cd0ab05d1bc46be693666d74d4693d187bb65c6
Xilicie <108251562+Xilicie@users.noreply.github.com>
2025-04-13 20:18:03 +0900

    Update heap_sort.py

--- assignment1/heap_sort.py
+++ assignment1/heap_sort.py
@@ -1,1 +1,31 @@
+def heap_sort(arr):
+    n = len(arr)
 
+    # Build Max-heap
+    for i in range(n // 2, -1, -1):
+        heapify(arr, n, i)
+
+    for i in range(n - 1, 0, -1):
+        # Swap
+        arr[i], arr[0] = arr[0], arr[i]
+
+        # Heapify root element
+        heapify(arr, i, 0)
+
+
+def heapify(arr, n, i):
+    largest = i
+    left = 2 * i + 1
+    right = 2 * i + 2
+
+    # Find largest among root and children
+    if left < n and arr[left] > arr[largest]:
+        largest = left
+
+    if right < n and arr[right] > arr[largest]:
+        largest = right
+
+    # If root is not largest
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+        heapify(arr, n, largest)
