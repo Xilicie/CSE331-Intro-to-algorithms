1272d4d9ec86043fed3827a1718dd3bddcaabde8
Xilicie <108251562+Xilicie@users.noreply.github.com>
2025-04-14 21:54:11 +0900

    Update selection_sort.py

--- assignment1/selection_sort.py
+++ assignment1/selection_sort.py
@@ -1,1 +1,16 @@
+def selection_sort(arr):
+    n = len(arr)
 
+    for i in range(n):
+        # Assume the current index holds the smallest value
+        min_index = i
+
+        # Search for the smallest value in the rest of the array
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+
+        # Swap the smallest found value with the value at index i
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+
+    return
