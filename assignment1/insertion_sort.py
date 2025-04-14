f451ed3ca88eac521d39d5afd87d952bd93266ce
Xilicie <108251562+Xilicie@users.noreply.github.com>
2025-04-14 21:20:31 +0900

    Update insertion_sort.py

--- assignment1/insertion_sort.py
+++ assignment1/insertion_sort.py
@@ -1,1 +1,14 @@
+def insertion_sort(arr):
+    n = len(arr)
 
+    # i-loop increasing, we do j-loop for every element in arr
+    for i in range(1, n):
+        # j-loop decreasing, checks if current i-element is in the correct spot
+        for j in range(i, 0, -1):
+            if arr[j - 1] > arr[j]:
+                # swap if order is inproper
+                arr[j - 1], arr[j] = arr[j], arr[j - 1]
+            else:
+                # break if every element we passed is in the correct order
+                break
+    return
