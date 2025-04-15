a30e2688de8d6a8208eba6041b8e3609b25da8cd
Xilicie <108251562+Xilicie@users.noreply.github.com>
2025-04-15 15:48:50 +0900

    Update comb_sort.py

--- assignment1/comb_sort.py
+++ assignment1/comb_sort.py
@@ -1,1 +1,16 @@
+def comb_sort(arr):
+    n = len(arr)
+    gap = n
+    shrink = 1.3
+    sorted = False
 
+    while not sorted:
+        gap = int(gap / shrink)
+        if gap <= 1:
+            gap = 1
+            sorted = True
+
+        for i in range(n - gap):
+            if arr[i] > arr[i + gap]:
+                arr[i], arr[i + gap] = arr[i + gap], arr[i]
+                sorted = False
