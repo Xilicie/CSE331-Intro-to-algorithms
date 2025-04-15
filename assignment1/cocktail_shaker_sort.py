d30cd5d66de8a497549c0d6cf69f87187557e23d
Xilicie <108251562+Xilicie@users.noreply.github.com>
2025-04-15 15:34:16 +0900

    Update cocktail_shaker_sort.py

--- assignment1/cocktail_shaker_sort.py
+++ assignment1/cocktail_shaker_sort.py
@@ -1,1 +1,23 @@
+def cocktail_shaker_sort(arr):
+    n = len(arr)
+    start = 0
+    end = n - 1
+    swapped = True
+
+    while swapped:
+        swapped = False
+
+        # Forward pass
+        for i in range(start, end):
+            if arr[i] > arr[i + 1]:
+                arr[i], arr[i + 1] = arr[i + 1], arr[i]
+                swapped = True
+        end -= 1
+
+        # Backward pass
+        for i in range(end, start, -1):
+            if arr[i] < arr[i - 1]:
+                arr[i], arr[i - 1] = arr[i - 1], arr[i]
+                swapped = True
+        start += 1
 
