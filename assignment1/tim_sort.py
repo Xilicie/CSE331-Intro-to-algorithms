1da16405c818bd12d9224f1685f057b07c779f07
Xilicie <108251562+Xilicie@users.noreply.github.com>
2025-04-15 15:07:37 +0900

    Update tim_sort.py

--- assignment1/tim_sort.py
+++ assignment1/tim_sort.py
@@ -1,1 +1,48 @@
+def insertion_sort(arr, left, right):
+    for i in range(left + 1, right + 1):
+        key = arr[i]
+        j = i - 1
+        while j >= left and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
+def merge(arr, l, m, r):
+    L = arr[l:m + 1]
+    R = arr[m + 1:r + 1]
+    i = j = 0
+    k = l
+    while i < len(L) and j < len(R):
+        if L[i] <= R[j]:
+            arr[k] = L[i]
+            i += 1
+        else:
+            arr[k] = R[j]
+            j += 1
+        k += 1
+    while i < len(L):
+        arr[k] = L[i]
+        i += 1
+        k += 1
+    while j < len(R):
+        arr[k] = R[j]
+        j += 1
+        k += 1
+
+def tim_sort(arr):
+    n = len(arr)
+    min_run = 32
+
+    for start in range(0, n, min_run):
+        end = min(start + min_run - 1, n - 1)
+        insertion_sort(arr, start, end)
+
+    size = min_run
+    while size < n:
+        for left in range(0, n, 2 * size):
+            mid = min(n - 1, left + size - 1)
+            right = min((left + 2 * size - 1), n - 1)
+            if mid < right:
+                merge(arr, left, mid, right)
+        size *= 2
 
